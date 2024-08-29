fc81775758476fe0360b95c756aabe98d82f6210
Paul Guillen <paulguillen190@gmail.com>
2024-08-29 00:12:54 -0600

    Estilo Imagen GDELTCard.kt

--- app/src/main/java/com/devpaul/infoxperu/domain/ui/utils/ArticleImage.kt
+++ app/src/main/java/com/devpaul/infoxperu/domain/ui/utils/ArticleImage.kt
@@ -3,12 +3,9 @@ package com.devpaul.infoxperu.domain.ui.utils
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,8 +31,7 @@ fun ArticleImage(article: Article) {
         contentDescription = "Image Article",
         modifier = Modifier
             .fillMaxWidth()
-            .height(160.dp)
-            .clip(RoundedCornerShape(16.dp)),
+            .height(160.dp),
         contentScale = contentScale
     )
 }
