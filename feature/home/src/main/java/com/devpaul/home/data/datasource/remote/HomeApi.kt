b73a25b0eb664839b8931581f1d9a02329b6d120
Paul Guillen <paulguillen190@gmail.com>
2025-06-16 06:35:04 -0500

    Change url avoid mocks

--- feature/home/src/main/java/com/devpaul/home/data/datasource/remote/HomeApi.kt
+++ feature/home/src/main/java/com/devpaul/home/data/datasource/remote/HomeApi.kt
@@ -9,19 +9,19 @@ import retrofit2.http.GET
 
 internal interface HomeApi {
 
-    @GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
-    //@GET("http://192.168.100.137:3000/home/dollarQuote")
+    //@GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
+    @GET("http://192.168.100.137:3000/home/dollarQuote")
     suspend fun dollarQuote(): Response<DollarQuoteResponse>
 
-    @GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
-    //@GET("http://192.168.100.137:3000/home/uit")
+    //@GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
+    @GET("http://192.168.100.137:3000/home/uit")
     suspend fun uit(): Response<UITResponse>
 
-    @GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
-    //@GET("http://192.168.100.137:3000/home/section")
+    //@GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
+    @GET("http://192.168.100.137:3000/home/section")
     suspend fun sections(): Response<SectionResponse>
 
-    @GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
-    //@GET("http://192.168.100.137:3000/home/gratitude")
+    //@GET("https://run.mocky.io/v3/df06e3d5-b9db-4596-9b4e-d1fa84d37fc7")
+    @GET("http://192.168.100.137:3000/home/gratitude")
     suspend fun gratitude(): Response<GratitudeResponse>
 }
